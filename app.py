af841dcd4203c4b9cc00e0343837d9c89e0b5a4d
Rugvedrc <144464437+Rugvedrc@users.noreply.github.com>
2025-04-13 18:41:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -188,6 +188,6 @@ def change_password():
 
 
 # Run the Flask app
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
